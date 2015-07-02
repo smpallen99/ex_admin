309324584f02a31a14460a7a48e63ae38f89b8a2
Stephen M. Pallen <smpallen99@yahoo.com>
2015-07-02 16:52:18 -0400

    mix updates

--- mix.exs
+++ mix.exs
@@ -25,6 +25,20 @@ defmodule ExAdmin.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:phoenix, github: "smpallen99/phoenix", branch: "v10_warnings_fix", override: true},
+      {:cowboy, "~> 1.0"},
+      {:mariaex, github: "xerions/mariaex", override: true },
+      {:phoenix_ecto, "~> 0.1.0"},
+      {:factory_girl_elixir, "~> 0.1.1"},
+      {:pavlov, "~> 0.1.2", only: :test},
+      {:inflex, "~> 1.0"},
+      {:sass_elixir, "~> 0.0.1"}, 
+      {:ex_form, github: "smpallen99/ex_form"},
+      {:xain, github: "smpallen99/xain", override: true},
+      {:scrivener, "~> 0.4.0"}, 
+      {:csvlixir, "~> 1.0.0"},
+      {:exactor, "~>1.0.0"}, 
+    ]
   end
 end
