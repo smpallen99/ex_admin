526b416a2a7ce177da918516ff14f11e9d8469eb
Stephen M. Pallen <smpallen99@yahoo.com>
2016-04-04 21:39:25 -0400

    theme index page

--- lib/ex_admin/index.ex
+++ lib/ex_admin/index.ex
@@ -107,7 +107,6 @@ defmodule ExAdmin.Index do
   require Integer
   import ExAdmin.Utils
   import ExAdmin.DslUtils
-  #import UcxCallout.Admin.ViewHelpers, only: [get_route_path: 3]
   import ExAdmin.Helpers
   import Kernel, except: [div: 2, to_string: 1]
   use Xain
@@ -119,10 +118,8 @@ defmodule ExAdmin.Index do
     end
   end
 
-
   @doc """
   The index macro is used to customize the index page of a resource. 
-  
   """
   defmacro index(opts \\ [], do: block) do
 
@@ -143,20 +140,17 @@ defmodule ExAdmin.Index do
         opts = unquote(options)
         unquote(contents)
 
-        columns = var!(columns, ExAdmin.Show) |> Enum.reverse
         selectable = case var!(selectable_column, ExAdmin.Index) do
           nil -> false
           other -> other
         end
 
+        opts = Enum.into(opts, %{})
+        |> Map.put(:column_list, var!(columns, ExAdmin.Show) |> Enum.reverse)
+        |> Map.put(:selectable_column, selectable)
+
         markup do
-          cond do 
-            opts[:as] == :grid -> 
-              ExAdmin.Index.render_index_grid(var!(conn), page, scope_counts, var!(cell, ExAdmin.Index), opts)
-            true -> 
-              ExAdmin.Index.render_index_table(var!(conn), page, columns, 
-                 %{selectable_column: selectable}, scope_counts, var!(actions, ExAdmin.Index))
-          end
+          ExAdmin.Index.render_index_pages(var!(conn), page, scope_counts, var!(cell, ExAdmin.Index), opts)
         end
       end
     end
@@ -176,6 +170,27 @@ defmodule ExAdmin.Index do
     end
   end
 
+  @doc """
+  Define a grid cell for grid view.
+
+  ## Example
+   
+      index as: :grid, default: true, columns: 6 do
+        import Kernel, except: [div: 2]
+        cell fn(p) -> 
+          div ".box" do
+            div ".box-body" do
+              a href: get_route_path(conn, :show, p.id) do
+                img(src: ExAdminDemo.Image.url({p.image_file_name, p}, :thumb), height: 100)
+              end
+            end
+            div ".box-footer" do
+              a truncate(p.title), href: get_route_path(conn, :show, p.id)
+            end
+          end
+        end
+      end
+  """
   defmacro cell(fun) do
     quote do 
       var!(cell, ExAdmin.Index) = unquote(fun)
@@ -220,9 +235,12 @@ defmodule ExAdmin.Index do
           columns = Keyword.put columns, :id, %{link: true}
         end
 
+        opts = %{}
+        |> Map.put(:column_list, columns)
+        |> Map.put(:selectable_column, true)
+
         markup do
-          ExAdmin.Index.render_index_table(var!(conn), page, columns, 
-            %{selectable_column: true}, scope_counts, [])
+          ExAdmin.Index.render_index_pages(var!(conn), page, scope_counts, nil, opts)
         end
     end
   end
@@ -230,238 +248,96 @@ defmodule ExAdmin.Index do
   defp get_resource_fields([]), do: []
   defp get_resource_fields([resource | _]), do: resource.__struct__.__schema__(:fields)
 
-  def render_index_grid(conn, page, scope_counts, cell, opts) do
-    columns = Keyword.get opts, :columns, 3
-    # resources = page.entries
-    # fields = get_resource_fields resources
-    count = page.total_entries
+  @doc false
+  def render_index_pages(conn, page, scope_counts, cell, page_opts) do
     name = resource_model(conn) |> titleize |> Inflex.pluralize
-    order = ExQueb.get_sort_order(conn.params["order"]) 
-    href = get_route_path(conn, :index) <> "?order="
     defn = ExAdmin.get_registered_by_controller_route(conn.params["resource"])
-    batch_actions = not false in defn.batch_actions
-    scopes = defn.scopes
-    # selectable = selectable and batch_actions
-
     label = get_resource_label(conn) |> Inflex.pluralize
-    resource_model = conn.params["resource"]
-    div ".box" do
-      batch_action_form conn, batch_actions, scopes, resource_model, scope_counts, fn -> 
-      if count == 0 do
-        div ".blank_slate_container" do
-          span ".blank_slate" do
-            if conn.params["q"] do
-              text "No #{humanize label} found."
-            else
-              text "There are no #{humanize label} yet. "
-              if ExAdmin.has_action?(conn, defn, :new) do
-                a "Create one", href: get_route_path(conn, :new)
-              end
-            end
-          end
-        end
-      else
-        div ".paginated_collection" do
-          div ".paginated_collection_contents" do
-            div ".index_content" do
-              div ".container-fluid" do
-                col_width = Kernel.div 12, columns
-                Enum.chunk(page.entries, columns, columns, [nil])
-                |> Enum.each(fn(list) -> 
-                  div ".row" do 
-                    Enum.each(list, fn(item) -> 
-                      div ".col-md-#{col_width}.col-sm-#{col_width * 2}.col-xs-12" do
-                        if item do
-                          cell.(item)
-                        end
-                      end
-                    end)
-                  end
-                end)
+    count = page.total_entries
 
-                # table(".index_grid", border: "0", cellspacing: "0", 
-                #     cellpadding: "0", paginator: "true") do
-                #   tbody do
-                #     Enum.chunk(page.entries, columns, columns, [nil])
-                #     |> Enum.each(fn(list) -> 
-                #       tr do 
-                #         Enum.each(list, fn(item) -> 
-                #           td do
-                #             if item do
-                #               cell.(item)
-                #             end
-                #           end
-                #         end)
-                #       end
-                #     end)
-                #   end
-                # end # table          
-              end
-            end # .index_content
-          end
-          div "#index_footer" do
-            href 
-            |> build_scope_href(conn.params["scope"])
-            |> build_order_href(order)
-            |> build_filter_href(conn.params["q"])
-            |> ExAdmin.Paginate.paginate(page.page_number, page.page_size, page.total_pages, count, name)
-            download_links(conn)
-          end
+    opts = %{
+      columns: Map.get(page_opts, :columns, 3),
+      column_list: Map.get(page_opts, :column_list),
+      count: page.total_entries, 
+      name: name, 
+      order: ExQueb.get_sort_order(conn.params["order"]), 
+      href: get_route_path(conn, :index) <> "?order=",
+      defn: defn,
+      batch_actions: not false in defn.batch_actions,
+      scopes: defn.scopes, 
+      label: label, 
+      resource_model: conn.params["resource"], 
+      page: page, 
+      cell: cell,
+      scope_counts: scope_counts,
+      opts: page_opts,
+      resources: page.entries, 
+      selectable_column: page_opts[:selectable_column]
+    }
+    _render_index_page(conn, opts, page_opts)
+  end
+
+  defp _render_index_page(conn, opts, %{as: :grid}) do
+    Module.concat(conn.assigns.theme, Index).wrap_index_grid fn -> 
+      Module.concat(conn.assigns.theme, Index).batch_action_form conn, 
+          false, opts[:scopes], opts[:resource_model], opts[:scope_counts], fn -> 
+        if opts[:count] == 0 do
+          Module.concat(conn.assigns.theme, Index).blank_slate_page(conn, opts)
+        else
+          Module.concat(conn.assigns.theme, Index).paginated_collection_grid(conn, opts)
         end
-      end
       end
     end
   end
-  
-  @doc false
-  def render_index_table(conn, page, columns, %{selectable_column: selectable}, scope_counts, actions) do
-    resources = page.entries
-    fields = get_resource_fields resources
-    count = page.total_entries
-    name = resource_model(conn) |> titleize |> Inflex.pluralize
-    order = ExQueb.get_sort_order(conn.params["order"]) 
-    href = get_route_path(conn, :index) <> "?order="
-    defn = ExAdmin.get_registered_by_controller_route(conn.params["resource"])
-    batch_actions = not false in defn.batch_actions
-    scopes = defn.scopes
-    selectable = selectable and batch_actions
+  defp _render_index_page(conn, opts, page_opts) do
+    page = opts[:page]
+    actions = opts[:actions]
+    opts = Map.put(opts, :fields, get_resource_fields page.entries)
+    selectable = opts[:selectable_column] and opts[:batch_actions]
+    columns = page_opts[:column_list]
+
     columns = unless Enum.any? columns, &((elem &1, 0) == "Actions") or is_nil(actions) do
       columns ++ [{"Actions", %{fun: fn(resource) -> build_index_links(conn, resource, actions) end}}]
     else
       columns
     end
 
-    label = get_resource_label(conn) |> Inflex.pluralize
-    resource_model = conn.params["resource"]
-
-    div ".box" do
-      batch_action_form conn, batch_actions, scopes, resource_model, scope_counts, fn -> 
-        if count == 0 do
-          div ".blank_slate_container" do
-            span ".blank_slate" do
-              if conn.params["q"] do
-                text "No #{humanize label} found."
-              else
-                text "There are no #{humanize label} yet. "
-                if ExAdmin.has_action?(conn, defn, :new) do
-                  a "Create one", href: get_route_path(conn, :new)
-                end
-              end
-            end
-          end
+    Module.concat(conn.assigns.theme, Index).wrap_index_grid fn -> 
+      Module.concat(conn.assigns.theme, Index).batch_action_form conn, 
+          opts[:batch_actions], opts[:scopes], opts[:resource_model], opts[:scope_counts], fn -> 
+        if opts[:count] == 0 do
+          Module.concat(conn.assigns.theme, Index).blank_slate_page(conn, opts)
         else
-          div ".box-body.table-responsive.no-padding" do
-            # div ".row" do
-            #   div ".col-sm-12" do
-                 div ".paginated_collection" do
-                  table(".table-striped.index.table.index_table") do
-                    ExAdmin.Table.table_head(columns, %{selectable: true, path_prefix: href, 
-                      sort: "desc", order: order, fields: fields, page: page,
-                      filter: build_filter_href("", conn.params["q"]),
-                      selectable_column: selectable})
-                    build_table_body(conn, resources, columns, %{selectable_column: selectable})
-                  end # table          
-                 end
-              # end # .index_content
-            # end
-          end
-          div ".box-footer.clearfix" do
-            href 
-            |> build_scope_href(conn.params["scope"])
-            |> build_order_href(order)
-            |> build_filter_href(conn.params["q"])
-            |> ExAdmin.Paginate.paginate(page.page_number, page.page_size, page.total_pages, count, name)
-            download_links(conn)
-          end
+          Module.concat(conn.assigns.theme, Index).paginated_collection_table(conn, opts)
         end
       end
     end
   end
 
-  defp build_scope_href(href, nil), do: href
-  defp build_scope_href(href, scope) do
+  @doc """
+  Build the scope link.
+  """
+  def build_scope_href(href, nil), do: href
+  def build_scope_href(href, scope) do
     String.replace(href, "?", "?scope=#{scope}&")
   end
 
-  defp build_order_href(href, {name, sort}), do: href <> "#{name}_#{sort}"
-  defp build_order_href(href, _), do: href
+  @doc """
+  Build the order link.
+  """
+  def build_order_href(href, {name, sort}), do: href <> "#{name}_#{sort}"
+  def build_order_href(href, _), do: href
 
-  defp build_filter_href(href, nil), do: href
-  defp build_filter_href(href, q) do 
+  @doc """
+  Build the filter link.
+  """
+  def build_filter_href(href, nil), do: href
+  def build_filter_href(href, q) do 
     q
     |> Map.to_list
     |> Enum.reduce(href, fn({name, value}, acc) -> 
       acc <> "&q%5B" <> name <> "%5D=" <> value
     end)
-  end
-
-  @doc false
-  def batch_action_form conn, enabled?, scopes, name, scope_counts, fun do
-    msg = "Are you sure you want to delete these #{name}? You wont be able to undo this."
-    scopes = unless Application.get_env(:ex_admin, :scopes_index_page, true), do: [], else: scopes
-    # enabled? = false
-    if enabled? or scopes != [] do
-      form "#collection_selection", action: "/admin/#{name}/batch_action", method: :post, "accept-charset": "UTF-8" do
-        div style: "margin:0;padding:0;display:inline" do
-          csrf = Plug.CSRFProtection.get_csrf_token
-          input name: "utf8", type: :hidden, value: "✓"
-          input(type: :hidden, name: "_csrf_token", value: csrf)
-        end
-        input "#batch_action", name: "batch_action", type: :hidden
-        div ".box-header" do
-          div ".table_tools" do
-            if enabled? do
-              div "#batch_actions_selector.dropdown_menu" do
-                # button ".disabled.dropdown_menu_button.dropdown-toggle.btn.btn-sm.btn-default Batch Actions ", "data-toggle": "dropdown" do
-                #   span ".fa.fa-caret-down"
-                # end
-                # ul ".dropdown-menu" do
-                #   li do
-                #     a ".batch_action Delete Selected", href: "#", "data-action": :destroy, "data-confirm": msg
-                #   end
-                # end
-                button ".disabled.dropdown_menu_button.btn.btn-xs.btn-default Batch Actions " do
-                  span ".fa.fa-caret-down"
-                end
-                div ".dropdown_menu_list_wrapper", style: "display: none;" do
-                  div ".dropdown_menu_nipple"
-                  ul ".dropdown_menu_list" do
-                    li do
-                      a ".batch_action Delete Selected", href: "#", "data-action": :destroy, "data-confirm": msg
-                    end
-                  end
-                end
-              end
-            end
-            if scopes != [] do
-              current_scope = ExAdmin.Query.get_scope scopes, conn.params["scope"]
-              #ul ".scopes.table_tools_segmented_control", style: "width: calc((100% - 10px) - 108px); float: right;" do
-              div ".btn-group", style: "width: calc((100% - 10px) - 108px); float: right;" do
-                for {name, _opts} <- scopes do
-                  count = scope_counts[name]
-                  selected = if "#{name}" == "#{current_scope}", do: ".selected", else: ""
-                  # li ".scope.#{name}#{selected}" do
-                    a ".table_tools_button.btn-sm.btn.btn-default", href: get_route_path(conn, :index) <> "?scope=#{name}" do
-                      # button type: :button, class: "btn btn-default" do
-                        text ExAdmin.Utils.humanize("#{name} ")
-                        span ".badge.bg-blue #{count}"
-                      # end
-                    end
-                  # end
-                end
-              end
-            end
-          end
-        end
-        div ".box-body" do
-          fun.()
-        end
-      end
-    else
-      div ".box-body" do
-        fun.()
-      end
-    end
   end
 
   @doc false
@@ -481,47 +357,9 @@ defmodule ExAdmin.Index do
     Inflex.parameterize(string, seperator)
   end
 
-
-  @doc false
-  def build_table_body(_conn, [], _columns, _opts) do
-    tbody
-  end
-  def build_table_body(conn, resources, columns, opts) do
-    model_name = resources |> List.first |> Map.get(:__struct__)
-    selectable = Map.get opts, :selectable_column
-
-    tbody do
-      Enum.with_index(resources) 
-      |> Enum.map(fn{resource, inx} -> 
-        odd_even = if Integer.is_even(inx), do: "even", else: "odd"
-        id = resource.id
-        tr(".#{odd_even}##{model_name}_#{id}") do
-          if selectable do
-            td(".selectable") do
-              div(".resource_selection_cell") do
-                input(".collection_selection#batch_action_item_#{id}", type: :checkbox, value: "#{id}",
-                  name: "collection_selection[]")
-              end
-            end
-          end
-          for field <- columns do
-            build_field(resource, conn, field, fn(contents, field_name) -> 
-              ExAdmin.Table.handle_contents(contents, field_name)
-            end)
-          end
-        end # tr
-      end)
-    end
-  end
-
-  # TODO: don't like that we can't handle the do block :(
-
   @doc false
   def build_index_links(conn, resource, actions) do
-    # name = controller_name(conn)
     resource_model = resource.__struct__
-    base_class = "member_link"
-    id = resource.id
 
     links = case actions do
       [] -> [:show, :edit, :destroy]
@@ -529,29 +367,11 @@ defmodule ExAdmin.Index do
       other -> other
     end
 
-    get_authorized_links(conn, resource_model)
+    list = get_authorized_links(conn, resource_model)
     |> Enum.filter(&(&1 in links))
     |> Enum.reverse
-    |> Enum.reduce([], fn(item, acc) -> 
-      link = case item do
-        :show -> 
-          a("View", href: get_route_path(conn, :show, id), class: base_class <> " view_link")
-        :edit -> 
-          a("Edit", href: get_route_path(conn, :edit, id), class: base_class <> " edit_link")
-        :destroy -> 
-          a("Delete", href: get_route_path(conn, :delete, id), 
-              class: base_class <> " delete_link", "data-confirm": confirm_message, 
-              "data-csrf": Plug.CSRFProtection.get_csrf_token,
-              "data-method": :delete, rel: :nofollow )
-      end
-      [link | acc]
-    end)
-    |> case do
-      [] -> []
-      list -> 
-        [{"Actions", list}]
-    end
 
+    Module.concat(conn.assigns.theme, Index).handle_action_links(list, conn, resource)
   end
 
   @doc false
--- lib/ex_admin/table.ex
+++ lib/ex_admin/table.ex
@@ -121,54 +121,50 @@ defmodule ExAdmin.Table do
   def build_th({_field_name, %{label: label} = opts}, table_opts) when is_binary(label), 
     do: build_th(label, opts, table_opts)
   def build_th({field_name, _opts}, _table_opts) when is_binary(field_name), 
-    do: th("#{field_name}")
-    # do: th(".#{Inflex.parameterize field_name, "_"} #{field_name}")
+    do: th(".#{Inflex.parameterize field_name, "_"} #{field_name}")
   def build_th(field_name, _),
-    do: th("#{humanize field_name}")
-    # do: th(".#{field_name} #{humanize field_name}")
+    do: th(".#{field_name} #{humanize field_name}")
   def build_th(field_name, opts, %{fields: fields} = table_opts) do
+    # require Logger
+    # Logger.warn "field_name: #{inspect field_name}, fields: #{inspect fields}"
     if String.to_atom(field_name) in fields and opts in [%{}, %{link: true}] do
       _build_th(field_name, opts, table_opts)
     else
-      th("#{humanize field_name}") 
-      # th(".#{field_name} #{humanize field_name}") 
+      th(".#{field_name} #{humanize field_name}") 
     end
   end
   def build_th(field_name, _, _) when is_binary(field_name) do
-    # th(class: to_class(field_name)) do
-    th do
+    th(class: to_class(field_name)) do
+    # th do
       text field_name
     end
   end 
   def build_th(field_name, _, _), do: build_th(field_name, nil)
 
-
-  # def _build_th(field_name, _opts, %{path_prefix: path_prefix, order: {name, sort}, 
-  #     fields: _fields} = table_opts) when field_name == name do
-  #   link_order = if sort == "desc", do: "asc", else: "desc"
-  #   page_segment = case Map.get table_opts, :page, nildo
-  #     nil -> ""
-  #     page -> "&page=#{page.page_number}"
-  #   end
-  #   th(".sortable.sorted-#{sort}.#{field_name}") do
-  #     a("#{humanize field_name}", href: path_prefix <> 
-  #       field_name <> "_#{link_order}#{page_segment}" <> 
-  #       Map.get(table_opts, :filter, ""))
-  #   end
-  # end
-  # def _build_th(field_name, _opts, %{path_prefix: path_prefix} = table_opts) do
-  def _build_th(field_name, _opts, _table_opts) do
-    th humanize(field_name)
-    # sort = Map.get(table_opts, :sort, "asc")
-    # page_segment = case Map.get table_opts, :page, nil do
-    #   nil -> ""
-    #   page -> "&page=#{page.page_number}"
-    # end
-    # th(".sortable.#{field_name}") do
-    #   a("#{humanize field_name}", href: path_prefix <> 
-    #     field_name <> "_#{sort}#{page_segment}" <> 
-    #     Map.get(table_opts, :filter, ""))
-    # end
+  def _build_th(field_name, _opts, %{path_prefix: path_prefix, order: {name, sort}, 
+      fields: _fields} = table_opts) when field_name == name do
+    link_order = if sort == "desc", do: "asc", else: "desc"
+    page_segment = case Map.get table_opts, :page, nil do
+      nil -> ""
+      page -> "&page=#{page.page_number}"
+    end
+    th(".sortable.sorted-#{sort}.#{field_name}") do
+      a("#{humanize field_name}", href: path_prefix <> 
+        field_name <> "_#{link_order}#{page_segment}" <> 
+        Map.get(table_opts, :filter, ""))
+    end
+  end
+  def _build_th(field_name, _opts, %{path_prefix: path_prefix} = table_opts) do
+    sort = Map.get(table_opts, :sort, "asc")
+    page_segment = case Map.get table_opts, :page, nil do
+      nil -> ""
+      page -> "&page=#{page.page_number}"
+    end
+    th(".sortable.#{field_name}") do
+      a("#{humanize field_name}", href: path_prefix <> 
+        field_name <> "_#{sort}#{page_segment}" <> 
+        Map.get(table_opts, :filter, ""))
+    end
   end
   def handle_contents(%Ecto.DateTime{} = dt, field_name) do
     td class: to_class(field_name) do
