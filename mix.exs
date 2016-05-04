4ac0d454bacd77033f36ee2f4c481f0e7429a629
Stephen M. Pallen <smpallen99@yahoo.com>
2016-05-04 12:45:56 -0400

    Update scrivener dep to scrivener_ecto. update mix.lock. Closes #88

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule ExAdmin.Mixfile do
       {:factory_girl_elixir, "~> 0.1.1"},
       {:inflex, "~> 1.5"},
       {:xain, "~> 0.5.2"},
-      {:scrivener, "~> 1.1.4"},
+      {:scrivener_ecto, github: "drewolson/scrivener_ecto"},
       {:csvlixir, "~> 1.0.0"},
       {:exactor, "~> 2.2.0"},
       {:ex_doc, "~> 0.11", only: :dev},
