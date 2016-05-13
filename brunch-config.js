677f4b5518bdceb5832c6a729a9da936be64aeba
Roman Smirnov <POMAHC@gmail.com>
2016-05-13 20:23:37 +0300

    Restore select2 plugin. #109

--- brunch-config.js
+++ brunch-config.js
@@ -55,11 +55,11 @@ exports.config = {
         "web/static/vendor/themes/admin_lte2/css/ionicons.min.css",
         "web/static/vendor/themes/admin_lte2/plugins/datepicker/datepicker3.css",
         "web/static/vendor/themes/admin_lte2/plugins/daterangepicker/daterangepicker-bs3.css",
-        "web/static/vendor/themes/admin_lte2//plugins/select2/select2.css",
+        "web/static/vendor/themes/admin_lte2/plugins/select2/select2.css",
       ],
       "css/active_admin.css.css": [
         "web/static/vendor/themes/active_admin/css/active_admin.css.css",
-        "web/static/vendor/themes/admin_lte2//plugins/select2/select2.css",
+        "web/static/vendor/themes/admin_lte2/plugins/select2/select2.css",
       ]
     }
     },
