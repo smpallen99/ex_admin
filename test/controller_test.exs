c44acc909c33cbed04ccd92332a54b89c5314797
Roman Smirnov <POMAHC@gmail.com>
2016-05-21 17:07:37 +0300

    Update controller tests. #115

--- test/controller_test.exs
+++ test/controller_test.exs
@@ -18,30 +18,30 @@ defmodule ExAdminTest.ControllerTest do
 
   test "lists noids", %{user: user} do
     insert_noid(user_id: user.id, name: "controller 1")
-    conn =  get conn(), get_route_path(%Noid{}, :index)
+    conn =  get conn(), admin_resource_path(Noid, :index)
     assert html_response(conn, 200) =~ ~r/Noids/
   end
 
   test "shows correct name for column name with space", %{user: user} do
     insert_noid(%{user_id: user.id, name: "Road Runner", company: "Acme"})
-    conn = get conn(), get_route_path(%User{}, :show, user.id)
+    conn = get conn(), admin_resource_path(user, :show)
     assert html_response(conn, 200) =~ ~r/User/
     assert String.contains?(conn.resp_body, ">Road Runner (Acme)<")
   end
 
   test "shows multiple table_for and markup_contents sections", %{user: user} do
-    conn = get conn(), get_route_path(%User{}, :show, user.id)
+    conn = get conn(), admin_resource_path(user, :show)
     assert conn.resp_body =~ ~r(<h3>First table</h3>.*<p>With some No-ID entries</p>.*<table.*</table>.*<table.*</table>.*<h3>\^\^ Second table</h3>)sm
   end
 
   test "shows new product page" do
-    conn = get conn(), get_route_path(%Product{}, :new), []
+    conn = get conn(), admin_resource_path(Product, :new), []
     assert html_response(conn, 200) =~ "New Product"
   end
 
   @invalid_attrs %{}
   test "does not create resource and renders errors when data is invalid" do
-    conn = post conn(), get_route_path(%Product{}, :create), product: @invalid_attrs
+    conn = post conn(), admin_resource_path(Product, :create), product: @invalid_attrs
     assert html_response(conn, 200) =~ "New Product"
     assert String.contains?(conn.resp_body, "can't be blank")
   end
@@ -57,22 +57,22 @@ defmodule ExAdminTest.ControllerTest do
   end
 
   test "shows 404 for GET missing resource" do
-    conn = get conn(), get_route_path(%User{}, :show, @wrong_resource_id)
+    conn = get conn(), admin_resource_path(%User{id: @wrong_resource_id}, :show)
     assert html_response(conn, 404) =~ ~r/not found/
   end
 
   test "shows 404 for PATCH missing resource" do
-    conn = patch conn(), get_route_path(%User{}, :edit, @wrong_resource_id)
+    conn = patch conn(), admin_resource_path(%User{id: @wrong_resource_id}, :edit)
     assert html_response(conn, 404) =~ ~r/not found/
   end
 
   test "shows 404 for PUT missing resource" do
-    conn = put conn(), get_route_path(%User{}, :edit, @wrong_resource_id)
+    conn = put conn(), admin_resource_path(%User{id: @wrong_resource_id}, :edit)
     assert html_response(conn, 404) =~ ~r/not found/
   end
 
   test "shows 404 for DELETE missing resource" do
-    conn = delete conn(), get_route_path(%User{}, :delete, @wrong_resource_id)
+    conn = delete conn(), admin_resource_path(%User{id: @wrong_resource_id}, :destroy)
     assert html_response(conn, 404) =~ ~r/not found/
   end
 
@@ -82,7 +82,7 @@ defmodule ExAdminTest.ControllerTest do
 
   test "batch selection", %{user: user} do
     params = batch_action_args "users", user.id
-    conn = post conn(), "/admin/users/batch_action", params
+    conn = post conn(), admin_resource_path(User, :batch_action), params
     assert html_response(conn, 302)
   end
 
