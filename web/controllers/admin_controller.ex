abf8e294cdd8725d3288e4c7fdde5e2a2b16f4c0
Roman Smirnov <POMAHC@gmail.com>
2016-05-22 21:46:20 +0300

    Remove redundant assigns to conn.

--- web/controllers/admin_controller.ex
+++ web/controllers/admin_controller.ex
@@ -9,17 +9,15 @@ defmodule ExAdmin.AdminController do
 
   def dashboard(conn, params) do
     defn = get_registered_by_controller_route!(conn, "dashboard")
-    contents = defn.__struct__ |> apply(:page_view, [conn])
+    conn =  assign(conn, :defn, defn)
+    contents = defn.__struct__.page_view(conn)
 
-    conn
-    |> assign(:defn, defn)
-    |> assign(:scope_counts, [])
-    |> render("admin.html", html: contents, defn: defn, resource: nil,
+    render(conn, "admin.html", html: contents, resource: nil, scope_counts: [],
       filters: (if false in defn.index_filters, do: false, else: defn.index_filters))
   end
 
   def select_theme(conn, %{"id" => id} = params) do
-    {id, _} = Integer.parse(params["id"])
+    {id, _} = Integer.parse(id)
     {_, theme} = Application.get_env(:ex_admin, :theme_selector, []) |> Enum.at(id)
     loc = Map.get(params, "loc", admin_path) |> URI.parse |> Map.get(:path)
 
