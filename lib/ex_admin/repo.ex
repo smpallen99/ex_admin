24ab2fc40c6b4555beac267739394290e6852e1b
Antoine Chauvin <blackrushx@gmail.com>
2016-09-03 13:15:00 +0200

    Fix form has_many

--- lib/ex_admin/repo.ex
+++ lib/ex_admin/repo.ex
@@ -326,6 +326,8 @@ res
   def get_assoc_join_model(resource, field) do
     res_model = resource.__struct__
     case res_model.__schema__(:association, field) do
+      %Ecto.Association.Has{queryable: queryable} ->
+        {:ok, queryable}
       %{through: [first, second]} ->
         {:ok, {res_model.__schema__(:association, first).related, second}}
       _ ->
@@ -340,6 +342,8 @@ res
     case get_assoc_join_model(resource, field) do
       {:ok, {assoc, second}} ->
         {assoc.__schema__(:association, second).related, assoc}
+      {:ok, assoc_model} ->
+        {assoc_model, field}
       error ->
         error
     end
