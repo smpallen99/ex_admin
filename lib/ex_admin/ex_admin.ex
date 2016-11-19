a70913f6e69f114d5c0879635a145bab8480daa1
Roman Smirnov <POMAHC@gmail.com>
2016-11-19 14:22:14 +0300

    Added possibility to configure action labels.

--- lib/ex_admin/ex_admin.ex
+++ lib/ex_admin/ex_admin.ex
@@ -261,7 +261,7 @@ defmodule ExAdmin do
         |> Enum.reverse
       action when action in [:index, :edit] ->
         if Utils.authorized_action?(conn, action, resource_model) do
-          [{action, action_button(conn, defn, singular, action, :new, actions)}]
+          [{:new, action_button(conn, defn, singular, action, :new, actions)}]
         else
           []
         end
@@ -292,7 +292,8 @@ defmodule ExAdmin do
   def action_button(conn, defn, name, _page, action, actions, id \\ nil) do
     if action in actions do
       if ExAdmin.Utils.authorized_action?(conn, action, defn) do
-        [action_link(conn, name, action, id)]
+        action_name = defn.action_labels[action] || humanize(action)
+        [action_link(conn, "#{action_name} #{name}", action, id)]
       else
         []
       end
@@ -318,19 +319,13 @@ defmodule ExAdmin do
   end
 
   defp action_link(conn, name, :delete, _id) do
-    {button_name(name, :delete),
+    {name,
       [href: admin_resource_path(conn, :destroy),
         "data-confirm": Utils.confirm_message,
         "data-method": :delete, rel: :nofollow]}
   end
   defp action_link(conn, name, action, _id) do
-    {button_name(name, action),
-      [href: admin_resource_path(conn, action)]}
-  end
-
-  defp button_name(name, :destroy), do: button_name(name, :delete)
-  defp button_name(name, action) do
-    "#{humanize action} #{name}"
+    {name, [href: admin_resource_path(conn, action)]}
   end
 
   @doc false
