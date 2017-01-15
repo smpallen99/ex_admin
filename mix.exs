8cf4e0c0b9b7cc08f58a438bd4907ef7c63bbf00
Yinghai Zhao <zyinghai@gmail.com>
2017-01-15 13:05:05 -0500

    Update Phoenix_ecto version.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule ExAdmin.Mixfile do
       {:phoenix, ">= 1.2.1"},
       {:phoenix_html, "~> 2.6"},
       {:ecto, "~> 2.0"},
-      {:phoenix_ecto, "~> 3.0.0"},
+      {:phoenix_ecto, "~> 3.2.0"},
       {:postgrex, ">= 0.9.0", only: :test},
       {:floki, "~> 0.8", only: :test},
       {:cowboy, "~> 1.0"},
